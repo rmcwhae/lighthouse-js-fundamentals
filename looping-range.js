b5947c3a1bf7424311c50bfafd084ac01c3a0df3
Russell McWhae <russell.mcwhae@gmail.com>
2019-04-04 03:46:02 +0000

    Adds looping over range exercise

---
+++ looping-range.js
@@ -0,0 +1,17 @@
+function range(start, end, step) {
+  var outputarray = [];
+  if (start == undefined || end == undefined || step == undefined || start > end || step < 0) {
+    return outputarray;//return empty array
+  } else {
+    var repeat = start;
+    while (repeat <= end) {
+      outputarray.push(repeat);
+      repeat += step;
+    }
+    return outputarray;//return non-empty array
+  }
+}
+
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
