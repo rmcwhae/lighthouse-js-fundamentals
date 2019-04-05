dbe7c415f115182b5ed7546167dedf1e7d66731e
Russell McWhae <russell.mcwhae@gmail.com>
2019-04-05 04:12:10 +0000

    Adds Merge Array exercise using manual array concatenation and insertion sort

---
+++ mergearrays.js
@@ -0,0 +1,19 @@
+function merge(array1, array2) {
+  var sorted = [];
+  var smallestn = 0;
+  for(i = 0; i < array2.length; i++) {
+    array1.push(array2[i]);//combine the two arrays
+  }
+  for(var i = 0; i < array1.length; i++) {
+    var value = array1[i];
+    for(var j = i - 1; j > -1 && array1[j] > value; j--) {
+      array1[j + 1] = array1[j];
+    }
+    array1[j + 1] = value
+  }
+  return array1;
+}
+
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
