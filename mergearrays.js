394a165378e9ebbef4686daf53972b6cb25ea6ab
Russell McWhae <russell.mcwhae@gmail.com>
2019-04-10 01:40:23 +0000

    Added link for insertion sort algorithm

--- mergearrays.js
+++ mergearrays.js
@@ -2,7 +2,7 @@ function merge(array1, array2) {
   for(i = 0; i < array2.length; i++) {
     array1.push(array2[i]);//combine the two arrays
   }
-  for(var i = 0; i < array1.length; i++) {
+  for(var i = 0; i < array1.length; i++) {//insertion sort per https://hackernoon.com/programming-with-js-insertion-sort-1316df8354f5
     var value = array1[i];
     for(var j = i - 1; j > -1 && array1[j] > value; j--) {
       array1[j + 1] = array1[j];
