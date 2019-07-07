5eba20a4080a0a58029206e54179ef9cd9904968
Russell McWhae <russell.mcwhae@gmail.com>
2019-07-07 18:51:35 +0000

    Adds forEach loop for test array

--- functionExpression.js
+++ functionExpression.js
@@ -17,3 +17,16 @@ emotions("happy", function laugh(number) {
   }
   return output += "!";
 });
+
+var test = [12, 929, 11, 3, 199, 1000, 7, 1, 24, 37, 4,
+  19, 300, 3775, 299, 36, 209, 148, 169, 299,
+  6, 109, 20, 58, 139, 59, 3, 1, 139
+];
+
+// Write your code here
+test.forEach(function(elem, index) {
+   if(elem % 3 === 0) {
+    test[index] += 100;
+   }
+});
+console.log(test);
